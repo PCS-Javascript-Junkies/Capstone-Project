0310894b6d18e82793f751ae3994edfaf62c0d1d
hannaliebl <hanna@hannaliebl.com>
2014-07-30 19:34:34 -0700

    add initial scaffolding

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+var $ = require('jquery');
+var Backbone = require('backbone');
+Backbone.$ = $;
+
+var MainView = require('./views/main-view');
+
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'main'
+  },
+  main: function () {
+    this.mainView = new MainView();
+    this.mainView.render();
+  }
+});
+
+$(function () {
+  window.app = new Router();
+  Backbone.history.start();
+});
