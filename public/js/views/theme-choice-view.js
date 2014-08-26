e6c6649bc2eabfbdb554e76a6aeb66d4f41cc489
hannaliebl <hanna@hannaliebl.com>
2014-08-26 16:09:29 -0700

    add randomization to story view choice; could use refinement

--- public/js/views/theme-choice-view.js
+++ public/js/views/theme-choice-view.js
@@ -14,11 +14,23 @@ var ThemeChoiceView = Backbone.View.extend({
     var event = event.target.id;
     var clickedThemeId = event + "Theme";
     this.model.set({theme: clickedThemeId});
-    this.model.set({story: tree.current.theme[clickedThemeId].stories[0]})
-    tree.current = tree.current.theme[clickedThemeId].next;
-    this.loadQuestionView();
+    var self = this;
+    var callback = function() {
+      setTimeout(function() {
+        self.loadQuestionView();
+      }, 500);
+    }
+    this.pickStory(callback());
+  },
+  pickStory: function(callback) {
+    var pickedTheme = this.model.attributes.theme;
+    var max = 1;
+    var index = Math.floor(Math.random() * (max - 0 + 1)) + 0;
+    var pickedStory = tree.current.theme[pickedTheme].stories[index];
+    this.model.set({story: pickedStory});
   },
   loadQuestionView: function () { //generalized function that calls the generic question view
+    tree.current = tree.current.theme[this.model.attributes.theme].next;
     var questionView = new QuestionView({model: this.model});
     questionView.render();
   },
